08c2b72803cf8935a09290fb88885dd47b413032
BitterMango33 <bittermango33@protonmail.com>
2023-03-19 18:54:56 +0100

    Add direction logs for the arrow

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,37 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+/*
+const carousel__arrow = document.querySelectorAll(".carousel__arrow");
+let i = 0;
+
+function arrowClick(direction) {
+  if (i == 0 && direction < 0) {
+    i = slides.length;
+  } else if (i == slides.length && direction > 0) {
+    i = 0;
+  } else {    
+    i += direction;
+  }
+  console.log(slides[i]);
+}
+
+carousel__arrow[0].addEventListener("click", function() {arrowClick(-1)}, false);
+carousel__arrow[1].addEventListener("click", function() {arrowClick(1)}, false);
+*/
+
+const carouselArrow = document.querySelectorAll(".carousel__arrow");
+
+function arrowClick(direction) {
+    if (direction) {
+        console.log("right");
+        return 1;
+    } 
+    console.log("left");
+    return -1;
+}
+
+for (let index = 0; index < carouselArrow.length; index++) {
+    carouselArrow[index].addEventListener("click", function() {arrowClick(index)}, false);
+}
